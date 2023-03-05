0c82a8f4b1f9ee061b072ab11fec9c05ba77e572
Andres <andres.aguilarfl@gmail.com>
2023-03-05 10:42:13 -0600

    update

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question) 
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}), 
+        ('Date information', {'fields': ['pub_date'], 'classes':
+                              ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently') #displays header in topbar
+    list_filter = ['pub_date'] #adds a filter 
+    search_fields = ['question_text'] #adds search box for questions
+
+admin.site.register(Question, QuestionAdmin) 
+
 #import model of question so that question objects can be seen by the admin
 
 # Register your models here.
